48c16d86a3d93691eedd6342952b0e326b28da47
akbarhussain973 <akbarhussain973mkd@gmail.com>
2026-07-26 12:33:36 +0500

    chore: enable Express trust proxy for Render deployment

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
+app.set("trust proxy", 1);
 const mongoose = require("mongoose");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
